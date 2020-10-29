f05128ef664088b8de8d6787211e156b6faebe14
Vlpalamar <65705755+Vlpalamar@users.noreply.github.com>
2020-10-29 13:26:54 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Static = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Static
+            // 
+            this.Static.Location = new System.Drawing.Point(27, 26);
+            this.Static.Name = "Static";
+            this.Static.Size = new System.Drawing.Size(75, 23);
+            this.Static.TabIndex = 0;
+            this.Static.Text = "Push me";
+            this.Static.UseVisualStyleBackColor = true;
+            this.Static.Click += new System.EventHandler(this.Static_Click_1);
+            this.Static.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Static_MouseMove);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Static);
+            this.panel1.Location = new System.Drawing.Point(247, 203);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(144, 79);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 494);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Static;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+
+        Random rand;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rand = new Random();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+
+        
+
+        private void Static_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Static_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            
+               
+                panel1.Location = new Point(rand.Next(0,this.ClientSize.Width-panel1.Size.Width), rand.Next(0, this.ClientSize.Height-panel1.Size.Height));
+            
+        }
+    }
+    
+}
